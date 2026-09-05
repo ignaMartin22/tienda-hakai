7d7bbfd86c2ea27fb6c9338cfaf355deda841a03
Ignacio Martín <fibias2004@gmail.com>
2026-09-05 20:59:07 -0300

    Añadir configuración de proxy para compatibilidad con Vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,7 @@ require('dotenv').config();
 const app = express();
 app.use('/api/auth/login', loginLimiter);
 
+app.set('trust proxy', 1); // Para que funcione correctamente en Vercel
 // Middlewares
 app.use(cors());
 app.use(express.json());
